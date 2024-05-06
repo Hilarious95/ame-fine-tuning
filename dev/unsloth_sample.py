7e8ac35bc9bc011c42b3a462b7de00faea9d3070
Armani Sadeghi <arman@armansadeghi.com>
2024-05-06 02:19:53 -0700

    feat: Basic setup completed

--- dev/unsloth_sample.py
+++ dev/unsloth_sample.py
@@ -1,39 +1,68 @@
-from unsloth import FastLanguageModel
-import torch
-from trl import SFTTrainer
-from transformers import TrainingArguments
-from datasets import load_dataset
 import os
-from config import BASE_DIR
+
+import torch
+from datasets import load_dataset
+from transformers import TrainingArguments
+from trl import SFTTrainer
+from unsloth import FastLanguageModel
 
 max_seq_length = 2048
+local_file = os.path.join("data", "small_sample.json")
 
-local_file = os.path.join(BASE_DIR, "data", "small_sample.json")
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+# More models at https://huggingface.co/unsloth
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/llama-3-8b-bnb-4bit",
+    max_seq_length=max_seq_length,
+    dtype=torch.float16,
+    load_in_4bit=True,
+)
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+EOS_TOKEN = tokenizer.eos_token  # Must add EOS_TOKEN
+
+
+def formatting_prompts_func(examples):
+    instructions = examples["instruction"]
+    inputs = examples["input"]
+    outputs = examples["output"]
+    texts = []
+    for instruction, input, output in zip(instructions, inputs, outputs):
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = alpaca_prompt.format(instruction, input, output) + EOS_TOKEN
+        texts.append(text)
+    return {"text": texts, }
+
+
+pass
 
 dataset = load_dataset(
     path="json",
     data_files={"train": local_file},
     split="train")
-
-# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
-# More models at https://huggingface.co/unsloth
-
-
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="unsloth/llama-3-8b-bnb-4bit",
-    max_seq_length=max_seq_length,
-    dtype=None,
-    load_in_4bit=True,
-)
+dataset = dataset.map(formatting_prompts_func, batched=True, )
 
 # Do model patching and add fast LoRA weights
 model = FastLanguageModel.get_peft_model(
     model,
     r=16,
-    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj", ],
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
+                    "gate_proj", "up_proj", "down_proj", ],
     lora_alpha=16,
     lora_dropout=0,  # Supports any, but = 0 is optimized
     bias="none",  # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
     use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
     random_state=3407,
     max_seq_length=max_seq_length,
@@ -48,22 +77,24 @@ trainer = SFTTrainer(
     max_seq_length=max_seq_length,
     tokenizer=tokenizer,
     args=TrainingArguments(
-        per_device_train_batch_size=2,
+        per_device_train_batch_size=1,
         gradient_accumulation_steps=4,
         warmup_steps=10,
         max_steps=60,
-        fp16=not torch.cuda.is_bf16_supported(),
-        bf16=torch.cuda.is_bf16_supported(),
+        fp16=True,
+        bf16=False,
         logging_steps=1,
         output_dir="outputs",
         optim="adamw_8bit",
         seed=3407,
     ),
 )
+print("here-----")
 trainer.train()
-
-# Go to https://github.com/unslothai/unsloth/wiki for advanced tips like
-# (1) Saving to GGUF / merging to 16bit for vLLM
-# (2) Continued training from a saved LoRA adapter
-# (3) Adding an evaluation loop / OOMs
-# (4) Cutomized chat templates
+# Save the trained model
+output_dir = "outputs"
+model.save_pretrained(output_dir)
+trainer.save_model(output_dir)
+trainer.model.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
+print(f"Model and tokenizer saved to {output_dir}")
